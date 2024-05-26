b2a795f490980dfde9d475575442d78b3786d964
Landon <117694319+landon-2003@users.noreply.github.com>
2024-05-26 08:54:46 -0300

    atualização exercicios js

---
+++ aula12ex/ex014/javascript/script.js
@@ -0,0 +1,23 @@
+/*window.document.getElementById('msg')
+window.document.getElementById('imagem')*/
+
+function carregar(){
+    var data = new Date()
+    //var hora = data.getHours()
+    var hora = 13
+    window.document.getElementById('msg').innerHTML = `Agora são ${hora} horas.`
+
+    if(hora >= 6 && hora < 12){
+        window.document.getElementById('imagem').src = 'imagens/manha.png'
+        document.body.style.background = '#468bce'
+    }else if(hora >=12 && hora <19){
+        window.document.getElementById('imagem').src = 'imagens/tarde.png'
+        document.body.style.background = '#fa8b54' 
+    }else if (hora >=19 && hora <24){
+        window.document.getElementById('imagem').src = 'imagens/noite.png'
+        document.body.style.background = '#3d1146'
+    }else{
+        window.document.getElementById('imagem').src = 'imagens/madrugada.png'
+        document.body.style.background = '#1d2a3c'
+    }
+}
