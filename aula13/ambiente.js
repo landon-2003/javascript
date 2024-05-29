7e73c3b8de71ff2035df8fc44001032223169252
Landon <117694319+landon-2003@users.noreply.github.com>
2024-05-29 10:26:14 -0300

    estudo de repetição

---
+++ aula13/ambiente.js
@@ -0,0 +1,23 @@
+var c = 1
+do{
+    console.log(`Tudo bem?${c}`)
+    c++
+}while(c <= 6)
+
+/*
+var c = 1
+while(c <= 500){
+    console.log(`Tudo bem?${c}`)
+    c++
+}
+*/
+
+
+/*
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/
